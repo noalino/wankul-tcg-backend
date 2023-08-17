39afcb941ba15be667689ed1bf5d7a168aca99c2
Noalino <bgelineau.dev@gmail.com>
2023-08-17 14:58:53 +0200

    Change cards table columns order

--- config/db/initdb.sql
+++ config/db/initdb.sql
@@ -4,9 +4,9 @@ CREATE TABLE IF NOT EXISTS cards (
     id uuid DEFAULT gen_random_uuid() PRIMARY KEY,
     number INT NOT NULL,
     name TEXT NOT NULL,
-    artist smallint NOT NULL,
-    collection smallint NOT NULL,
-    effigy smallint,
     image TEXT NOT NULL,
+    collection smallint NOT NULL,
+    artist smallint NOT NULL,
+    effigy smallint,
     rarity smallint NOT NULL
 )
