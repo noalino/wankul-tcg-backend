385de69cd35d4b5383a14bfd9160426313b04eed
Noalino <bgelineau.dev@gmail.com>
2023-08-22 12:06:38 +0200

    Enable CORS

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,14 @@ import { AppModule } from './app.module';
 import { setupSwagger } from './swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
   const configService = app.get(ConfigService);
   const port = configService.get<number>('API_PORT');
 
   setupSwagger(app);
+
   await app.listen(port);
 }
 bootstrap();
